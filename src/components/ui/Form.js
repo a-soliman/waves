06cc5396cd9a0f02654118e389f545901d8304b9
a-soliman <ahmed.soliman@programmer.net>
2018-12-16 08:31:10 -0800

    edited the form component , making use of the new button component

--- src/components/ui/Form.js
+++ src/components/ui/Form.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import FormField from './FormField';
+import React from "react";
+import FormField from "./FormField";
+import Button from "./Button";
 
-const Form = ({ fields = [], onChangleHandler, onSubmitHandler }) => {
-    return (
-        <form onSubmit={onSubmitHandler}>
-            {fields.map((field, i) => <FormField key={i} field={field} onChangeHandler={onChangleHandler} />)}
-            <input type="submit" className="btn btn-info btn-block mt-4" />
-        </form>
-    );
+const Form = ({
+  fields = [],
+  onChangleHandler,
+  onSubmitHandler,
+  submitButtonTitle = "Submit"
+}) => {
+  return (
+    <form onSubmit={onSubmitHandler}>
+      {fields.map((field, i) => (
+        <FormField key={i} field={field} onChangeHandler={onChangleHandler} />
+      ))}
+
+      <Button
+        type="submitButton"
+        title={submitButtonTitle}
+        addStyles={{ width: "100%", textAlign: "center", border: "0" }}
+      />
+    </form>
+  );
 };
 
 export default Form;
