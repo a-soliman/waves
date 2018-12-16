eb0bd32f681de2a6d70ed45dc2700b5442fdbb18
a-soliman <ahmed.soliman@programmer.net>
2018-12-16 08:32:09 -0800

    updating the user dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import User from "./User";
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="full-height d-flex justify-content-center align-items-center">
-        <h1 className="display-2 mb-5">Dashboard</h1>
-        <h4>
-          hey <strong>{this.props.auth.user.name}</strong>,
-        </h4>
-        <p className="lead">
-          This is a private route, Can only be displayed and visited by logged
-          in users.
-        </p>
-      </div>
+      <User>
+        <div className="full-height d-flex justify-content-center align-items-center">
+          <h1 className="display-2 mb-5">Dashboard</h1>
+          <h4>
+            hey <strong>{this.props.auth.user.name}</strong>,
+          </h4>
+          <p className="lead">
+            This is a private route, Can only be displayed and visited by logged
+            in users.
+          </p>
+        </div>
+      </User>
     );
   }
 }
