47a6878ea37cd83942a648cceed4b4157e01220d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-17 08:56:43 -0800

    coded the home components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
-import { homedir } from "os";
+import { connect } from "react-redux";
+import HomeSlider from "./HomeSlider";
+import HomePromotion from "./HomePromotion";
+import {
+  getProductsByArrival,
+  getProductsBySell
+} from "../../actions/products";
 
 class Home extends Component {
+  componentDidMount = () => {
+    this.props.getProductsByArrival();
+    this.props.getProductsBySell();
+  };
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <HomeSlider />
+        <HomePromotion />
+      </div>
+    );
   }
 }
+const mapStateToProps = state => ({
+  auth: state.auth,
+  products: state.products,
+  errors: state.errors
+});
 
-export default Home;
+const mapDispatchToProps = dispatch => ({
+  getProductsByArrival: () => dispatch(getProductsByArrival()),
+  getProductsBySell: () => dispatch(getProductsBySell())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
