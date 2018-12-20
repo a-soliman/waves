0477568f6a236d1ee10b4163e608c43911a9b408
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-20 09:13:30 -0800

    added actions for brands and woods

--- src/actions/products.js
+++ src/actions/products.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import {
   GET_ERRORS,
   GET_PRODUCTS_BY_ARRIVAL,
-  GET_PRODUCTS_BY_SELL
+  GET_PRODUCTS_BY_SELL,
+  GET_BRANDS,
+  GET_WOODS
 } from "./types";
 import { history } from "../routers/AppRouter";
 
@@ -38,4 +40,18 @@ export const getProductsBySell = () => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const getBrands = () => dispatch => {
+  axios
+    .get("api/brands")
+    .then(res => dispatch({ type: GET_BRANDS, payload: res.data }))
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+export const getWoods = () => dispatch => {
+  axios
+    .get("api/woods")
+    .then(res => dispatch({ type: GET_WOODS, payload: res.data }))
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
